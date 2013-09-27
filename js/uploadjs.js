a1454108f93a50a733e7d14f90740ec2208906d0
013 <ryan@swiftler.com>
2013-09-27 12:09:38 +0200

    Added UNLICENSE and testing image upload preview

--- js/uploadjs.js
+++ js/uploadjs.js
@@ -5,8 +5,13 @@ function addFormField(Tvalue, title){
 	next = next + 1;
 	var newIn = '<input id="field' + next + '" name="imgHash' + next + '" type="hidden" value="' + Tvalue + '">';
 	newIn += '<span id="' + Tvalue + '" class="del" style="cursor: pointer;"> &times; </span>';
-	//newIn += '<br /><input class="span3" id="title' + next + '" name="imgTitle' + next + '" type="text" value="' + title + '">';
+	
+	newIn += '<img src="images/'
+			+Tvalue.substring(0,4)+'/'+Tvalue.substring(4,12)+'_thumb.'+title.split('.').reverse()[0]+
+			'" >';
+
 	newIn += '<span id="spantitle'+next+'" class="title '+next+'" >'+title+'</span><input name="imgTitle'+next+'" type="hidden" id="title'+next+'" value="'+title+'">';
+	
 	newIn += '<br /><input class="span3" id="tag' + next + '" name="imgTag' + next + '" type="text" value=""><br>';
 	var newInput = $(newIn);
 	$(addto).after(newInput);
--- test2.php
+++ test2.php
@@ -1,15 +1,11 @@
-<?
-
-
-?>
-
 <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+<!--
 <link rel="stylesheet" href="css/jquery-ui-1.10.3.custom.min.css">
 
+<script src="js/jquery-ui-1.10.3.custom.min.js"></script>-->
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<script src="js/jquery-ui-1.10.3.custom.min.js"></script>
 
-
+<!--
 <script type="text/javascript">
  $(function() {
 	 function split( val ) {
@@ -63,3 +59,47 @@
 <label for="birds">Tags: </label>
 <input id="birds" size="50" />
 </div>
+-->
+<script type="text/javascript">
+$(document).ready(function() {
+
+$('.hi').on('mouseover', function() {
+	    $('.showme').slideDown();
+});
+
+$('.hi').mouseout(function() {
+	    window.isoverdiv = false;
+	    setTimeout(function() {
+	        if (!window.isoverdiv) {
+		        $('.showme').slideUp();
+		    }
+	 }, 200);
+});
+
+$('.showme').mouseover(function() {
+	    window.isoverdiv = true;
+});
+
+});
+</script>
+<style>
+.hi { width:250px; height:187px; overflow: hidden; }
+.button {width:250px; height:187px; position:fixed; overflow: hidden; }
+.showme {width:245px; height:30px; position:fixed; background: rgba(25, 25, 25, 0.5); display:none; cursor: pointer; color: #fff; padding: 5px 0px 0px 5px; overflow: hidden; }
+</style>
+
+<div class="hi">
+<div class="button">
+<img style="z-index: -1;" src="http://ryan.swiftler.com/SwiftGallery/images/bdf3/bf1da340_thumb.jpg">
+</div>
+<div class="showme">
+     Title ~~~
+	 <span class="pull-right glyphicon glyphicon-remove-sign"></span>
+</div>
+</div>
+<!--
+<div>
+<span></span>
+</div>
+
+-->
